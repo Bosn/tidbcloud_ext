e0ed7980205844329c74a177ff2cebd89b4982d7
Bosn <bosn@outlook.com>
2023-07-19 14:24:46 +0800

    feat: basic funcs ready

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,19 @@
-const body = document.body
+  !function() {
 
-if (body) {
-  const div = document.createElement("div")
-  div.innerHTML = "<h1>TiDB Admin Extension Debug</h1>"
-  body.insertAdjacentElement("afterend", div)
-}
+    setTimeout(() => {
+      $('*[data-mp-event]').each((_, el) => {
+        const eventName = $(el).data('mp-event')
+        $(el).popover({
+          title: null,
+          content: `<div class="__ext_tooltip">
+                        <div class="__ext_tooltip_title">MixPanel event name</div>
+                        <div class="__ext_tooltip_value">${eventName}</div>
+                    </div>`,
+          trigger: 'hover',
+        })
+      })
+    }, 5000)
+
+  }()
+
+  

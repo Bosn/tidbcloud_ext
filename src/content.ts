a80621d386557fce77b8a3ec05c762f847e7ca71
Bosn <bosn@outlook.com>
2023-07-20 11:53:49 +0800

    feat: runnable with TypeScript

--- src/content.ts
+++ src/content.ts
@@ -1,27 +1,25 @@
 setTimeout(() => {
-  alert('test')
-  // $('*[data-mp-event]').each((_, el) => {
-  //   const eventName = $(el).data('mp-event')
-  //   const content = `
-  //       <div class="__ext_tooltip">
-  //         <div class="__ext_tooltip_title">MixPanel event name</div>
-  //         <div class="__ext_tooltip_value">
-  //           ${eventName}
-  //           <img
-  //             class="__ext_tooltip_icon"
-  //             src="${chrome.runtime.getURL('images/copy.png')}"
-  //             onClick="__copyTextToClipboard("${eventName}"); "
-  //           />
-  //         </div>
-  //       </div>
-  //       `
-  //   console.log(content)
-  //   $(el).popover({
-  //     title: null,
-  //     content,
-  //     trigger: 'hover',
-  //   })
-  // })
+  $('*[data-mp-event]').each((_, el) => {
+    const eventName = $(el).data('mp-event')
+    const content = `
+        <div class="__ext_tooltip">
+          <div class="__ext_tooltip_title">MixPanel event name</div>
+          <div class="__ext_tooltip_value">
+            ${eventName}
+            <img
+              class="__ext_tooltip_icon"
+              src="${chrome.runtime.getURL('images/copy.png')}"
+              onClick="__copyTextToClipboard("${eventName}"); "
+            />
+          </div>
+        </div>
+        `;
+    ($(el) as any).popover({
+      title: null,
+      content,
+      trigger: 'hover',
+    })
+  })
 }, 5000)
 
 
